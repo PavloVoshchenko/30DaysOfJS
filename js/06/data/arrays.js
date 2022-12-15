3124803eb2497f46fcb30d6c9b9e342a06a561a5
PavloVoshchenko <Pavlo_Voshchenko@epam.com>
2022-12-15 10:56:26 +0200

    06

---
+++ js/06/data/arrays.js
@@ -0,0 +1,11 @@
+const countriesArray = ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"];
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+];
